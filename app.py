b8a0df7f25f7518928c3bf149eb33d92fe64847e
sebranchett <s.e.branchett@tudelft.nl>
2022-07-12 17:33:21 +0200

    need environment to pass value to another stack, no synth yet

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
-from aws_cdk import App
+import os
+
+from aws_cdk import App, Environment
 
 from HubStacks.stable_stack import StableStack
 from HubStacks.hub_stack import HubStack
 
+default_env = Environment(
+    account=os.environ["CDK_DEFAULT_ACCOUNT"],
+    region=os.environ["CDK_DEFAULT_REGION"]
+)
+
 app = App()
-StableStack(app, "StableStack")
-HubStack(app, "HubStack")
+StableStack(app, "StableStack", env=default_env)
+HubStack(app, "HubStack", env=default_env)
 
 app.synth()
