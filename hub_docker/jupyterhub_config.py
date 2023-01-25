ac3d01bf6d4acc8b452f3151dc0da1aafde0b6a5
sebranchett <s.e.branchett@tudelft.nl>
2023-01-25 16:37:19 +0100

    explanation of full path to jupyterhub-singleuser command

--- hub_docker/jupyterhub_config.py
+++ hub_docker/jupyterhub_config.py
@@ -90,6 +90,9 @@ c.FargateSpawner.aws_region = os.environ.get('FARGATE_SPAWNER_REGION')
 c.FargateSpawner.aws_ecs_host = os.environ.get('FARGATE_SPAWNER_ECS_HOST')
 c.FargateSpawner.notebook_port = 8888
 c.FargateSpawner.notebook_scheme = "http"
+# #PATH seems to get mangled when starting a single user container from
+# jupyterhub container. /opt/conda/bin does note appear and this is
+# exactly what is needed for the command jupyterhub-singleuser
 c.FargateSpawner.get_run_task_args = lambda spawner: {
     'cluster': os.environ.get('FARGATE_SPAWNER_CLUSTER'),
     'taskDefinition': os.environ.get('FARGATE_SPAWNER_TASK_DEFINITION'),
